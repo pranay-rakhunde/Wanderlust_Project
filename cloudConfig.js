b8847c2e4c90806afb0d283e58c61c6f0e73294a
Pranay Rakhunde <prakhunde22@gmail.com>
2026-02-17 16:53:40 +0530

    done

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const multer = require("multer");
-const  {CloudinaryStorage}  = require('multer-storage-cloudinary');
+const CloudinaryStorage  = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
