10bb129a08c78191abb12f998615ddfe823d01a3
Pranay Rakhunde <prakhunde22@gmail.com>
2026-02-17 17:01:06 +0530

    done

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname,"public")));
 const store = MongoStore.create({
     mongoUrl :dbUrl,
     crypto:{
-        secret:process.env.SECERT,
+        secret:process.env.SECRET,
     },
     touchAfter : 24*3600,
 });
